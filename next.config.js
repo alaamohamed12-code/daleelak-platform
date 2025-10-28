b1dce393783f7d38f6e5d373d47b6bdc0f1d1b8d
Mahmoud Hammad <mahmoud@daleelak.com>
2025-10-28 18:57:40 +0300

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/daleelak-platform',
   images: {
+    unoptimized: true,
     domains: ['picsum.photos'],
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
